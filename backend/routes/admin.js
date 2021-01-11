ff77626bdbc1edbbc7d8c3ae2bdc474bb949cd17
saee <saee_saadat@outlook.com>
2021-01-11 05:27:22 +0330

    update + security mesures

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -3,29 +3,32 @@ const express = require('express');
 const admin = express.Router();
 const Post = Parse.Object.extend("Post");
 const authenticateToken = require("../middleware/tokenAuthMiddleware") ;
+const user = require('./user');
 
 admin.use(authenticateToken.authenticateToken);
 
-admin.post('/post/crud', async function (req, res) {
+admin.post('/post/crud', checkForTitleAndContent,  async (req, res) => {
   console.log("Handling posting a post!");
-  const body = req.body || { title: 'post title', content: 'post content'};
+  const body = req.body;
   Parse.User.enableUnsafeCurrentUser();
   
   const user = res.locals.user;
   const post = new Post();
   const postACL = new Parse.ACL();
   postACL.setPublicReadAccess(true);
+  postACL.setWriteAccess(user, true);
+  postACL.setReadAccess(user, true);
 
   post.setACL(postACL);
   post.set("title", body.title);
   post.set("content", body.content);
   post.set("createdBy", user);
-  post.save().then((postResp) => {
-    const post = postResp.toJSON();
-    res.status(200).send({"title": post.title, 
-    "content": post.content,
-    "createdBy": {"username": post.createdBy.username, "email": post.createdBy.email}                  
-  });
+  post.save().then((post) => {
+    res.status(200).send({"title": post.get("title"), 
+    "content": post.get("content"),
+    "createdBy": {"username": post.get("createdBy").get("username"), "email": post.get("createdBy").get("email")},
+    "id": post.id,
+    });
 
   }, function (error) {
     res.status(error.code).send(error.message);
@@ -34,5 +37,60 @@ admin.post('/post/crud', async function (req, res) {
 
 });
 
+
+
+admin.put('/post/crud/:id', checkForTitleAndContent, async (req, res) => {
+  console.log("Handling updating a post!");
+  
+  const id = req.params.id;
+  if (!id) {
+    res.status(400).send("خطا در url ارسالی");
+    return;
+  }
+  const body = req.body;
+  const user = res.locals.user;
+  Parse.User.enableUnsafeCurrentUser();
+
+  const query = new Parse.Query(Post);
+  query.get(id).then((post) => {
+
+    if(!post.getACL().getWriteAccess(user)) {
+      res.status(401).send("شما اجازه‌ی تغییر این پست را ندارید.");
+      return;
+    }    
+
+    post.set("title", body.title);
+    post.set("content", body.content);
+
+    post.save().then((postResp) => {
+      const post = postResp.toJSON();
+      res.status(200).send({"title": post.title, 
+      "content": post.content,
+      "createdBy": {"username": post.createdBy.username, "email": post.createdBy.email},
+      "id": post.objectId,
+      });
+  
+    }, function (error) {
+      console.log(error.message);
+      res.status(401).send("شما اجازه‌ی تغییر این پست را ندارید.");
+      return;
+    });
+
+  }, (error) => {
+    console.log(error.message);
+    res.status(400).send("پست درخواستی شما یافت نشد.");
+    return;
+  });
+
+})
+
+
+function checkForTitleAndContent(req, res, next) {
+  if (!req.body.title || !req.body.content) {
+    res.status(400).send("Request must contain Title and Content");
+    return;
+  }
+  next();
+}
 
 module.exports = admin;
